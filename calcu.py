adfbff88b9d4b72656373017aa827caff2d25f65
milten225 <45454701+milten225@users.noreply.github.com>
2018-11-29 19:30:31 +0300

    added features

---
+++ calcu.py
@@ -0,0 +1,20 @@
+print("Ноль в качестве знака операции завершит работу программы")
+while True:
+	s = input("Знак (+,-,*,/): ")
+	if s == '0': break
+	if s in ('+','-','*','/'):
+		num_1 = float(input("Введите первое число:"))
+		num_2 = float(input("Введите второе число:"))
+		if s == '+':
+			print("%.2f" % (num_1+num_2))
+		elif s == '-':
+			print("%.2f" % (num_1-num_2))
+		elif s == '*':
+			print("%.2f" % (num_1*num_2))
+		elif s == '/':
+			if y != 0:
+				print("%.2f" % (num_1/num_2))
+			else:
+				print("Деление на ноль!")
+	else:
+		print("Неверный знак операции!")
